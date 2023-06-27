c8cc1e355cc04cab994cd7afd6467801042948f9
Uzair Manzoor <uzair.kayani555@gmail.com>
2023-06-27 13:25:31 +0500

    update js file

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const mobieMenu = document.getElementById('mobile-nav-items');
+const mobileMenu = document.getElementById('mobile-nav-items');
 function openmenu() {
-  popup.style.display = 'block';
+  mobileMenu.style.display = 'block';
 }
 
 function closemenu() {
-  popup.style.display = 'none';
+  mobileMenu.style.display = 'none';
 }
-popup.addEventListener('click', openmenu);
-popup.addEventListener('click', closemenu);
+mobileMenu.addEventListener('click', openmenu);
+mobileMenu.addEventListener('click', closemenu);
