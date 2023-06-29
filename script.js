5da7f989e5ef88d1313236a4325f7dd7f594c5b2
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-06-29 15:04:51 +0500

    fixed eslint errors

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const mobileMenu = document.getElementById('mobile-nav-items');
 function openmenu() {
   mobileMenu.style.display = 'block';
 }
-
 function closemenu() {
   mobileMenu.style.display = 'none';
 }
-
 mobileMenu.addEventListener('click', openmenu);
 mobileMenu.addEventListener('click', closemenu);
 // POPUP WINDOW
 const popupWindow = document.getElementById('popup-window');
 function openPopup() {
   popupWindow.classList.add('open-popup');
+  popupWindow.style.display = 'block';
 }
 function closePopup() {
   popupWindow.classList.remove('open-popup');
+  popupWindow.style.display = 'none';
 }
+popupWindow.addEventListener('click', openPopup);
+popupWindow.addEventListener('click', closePopup);
